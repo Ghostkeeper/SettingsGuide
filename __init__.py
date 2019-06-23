7a11f7b9611653a71d7025716349184ad3ecc2f4
Ghostkeeper <rubend@tutanota.com>
2019-06-23 22:36:58 +0200

    Don't make Cura Settings Guide translateable
    There is no way for Cura's plug-ins to translate themselves at the moment.

--- __init__.py
+++ __init__.py
@@ -6,14 +6,10 @@
 
 from . import CuraSettingsGuide
 
-from UM.i18n import i18nCatalog
-i18n_catalog = i18nCatalog("cura")
-
-
 def getMetaData():
 	return {
 		"view": {
-			"name": i18n_catalog.i18nc("@item:inmenu", "Cura Settings Guide"),
+			"name": "Cura Settings Guide",
 			"weight": 0
 		}
 	}
