111e9b295cb46b718fe86a4369a1cc2ef3b0996c
Ghostkeeper <rubend@tutanota.com>
2019-12-07 22:56:34 +0100

    Add automated test to see if all image links are working
    Looks like I was diligent. Indeed all images are working correctly after the transformation I did just now.

---
+++ test/links.py
@@ -0,0 +1,45 @@
+#Copyright (C) 2019 Ghostkeeper
+#This plug-in is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#This plug-in is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for details.
+#You should have received a copy of the GNU Affero General Public License along with this plug-in. If not, see <https://gnu.org/licenses/>.
+
+"""
+These are tests to check whether there are any broken links in any articles.
+"""
+
+import os  # To find all articles.
+import re  # To find the links in the Markdown articles.
+import unittest  # Run the automated tests.
+
+class TestLinks(unittest.TestCase):
+	"""
+	These are tests to check whether there are any broken links in any articles.
+	"""
+
+	def all_articles(self):
+		"""
+		Get all article file paths.
+		:return: A sequence of all article file paths.
+		"""
+		for root, _, files in os.walk(os.path.join(os.path.dirname(__file__), "..", "resources", "articles")):
+			for filename in files:
+				_, extension = os.path.splitext(filename)
+				if extension != ".md":
+					continue  # Not an article. Ignore this.
+				yield os.path.join(root, filename)
+
+	def test_images(self):
+		"""
+		Test if the links to images are correct.
+		"""
+		find_images = re.compile(r"!\[.*\]\((.+)\)")
+		for filename in self.all_articles():
+			with self.subTest():
+				with open(filename) as f:
+					contents = f.read()
+				for image_link in find_images.findall(contents):
+					image_path = os.path.join(os.path.dirname(filename), image_link)
+					assert os.path.exists(image_path), "Article {article_path} refers to image {image_path}, which doesn't exist.".format(article_path=filename, image_path=image_link)
+
+if __name__ == "__main__":
+	unittest.main()
