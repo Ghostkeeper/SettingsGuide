bc49fd4490e0002d489dedb7275a0ef1c3445e98
Ghostkeeper <rubend@tutanota.com>
2020-03-16 00:45:19 +0100

    Fix getting language list before all languages are loaded
    The articles map is only filled after things are completely loaded. So get the language list from something that always gets loaded at the start: the article_locations map.

--- CuraSettingsGuide.py
+++ CuraSettingsGuide.py
@@ -404,4 +404,4 @@ class CuraSettingsGuide(Extension, QObject):
 		:param article_key: The article ID to get the languages for.
 		:return: A list of language codes (e.g. [nl_NL, en_US]).
 		"""
-		return list(self.articles[article_key].keys())
+		return list(self.article_locations[article_key].keys())
