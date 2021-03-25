5ca5bd7e88ba571e8d1aad31eddc94785e48b3cf
Ghostkeeper <rubend@tutanota.com>
2021-03-25 18:44:03 +0100

    Fix combining animations with transparent parts
    It needs to dispose of the previous layer such that things can become transparent again when they need to.

--- ScreenshotTool.py
+++ ScreenshotTool.py
@@ -75,7 +75,7 @@ commands = {
 	"reduce_palette": ["convert", "-colors", "{colours}", "+dither", "{input}", "png:{output}"],  # Reduce colour palette of an image.
 	"optimise_png1": ["optipng", "-o7", "-strip", "all", "-snip", "-out", "{output}", "{input}"],
 	"optimise_png2": ["ect", "-9", "-strip", "--pal_sort=16", "--allfilters", "{output}"],  # Reduce file size of PNG images.
-	"merge_gif": ["convert", "-colors", "{colours}", "+dither", "-delay", "{delay}", "-loop", "0", "{inputs}", "{output}"],  # Merge multiple images into a GIF.
+	"merge_gif": ["convert", "-colors", "{colours}", "+dither", "-delay", "{delay}", "-loop", "0", "-dispose", "previous", "{inputs}", "{output}"],  # Merge multiple images into a GIF.
 	"optimise_gif": ["flexigif", "-f", "{input}", "{output}"]  # Reduce file size of GIF images.
 }
 
