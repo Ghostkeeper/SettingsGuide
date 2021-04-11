e9b094dd61f2ee0fc778e44ef49e677a963aeaf9
Ghostkeeper <rubend@tutanota.com>
2021-04-11 04:26:04 +0200

    Add screenshot instructions for extensive stitching
    This setting needs a custom script to generate the 3D model since it's non-manifold. OpenSCAD refuses to generate models like this.
    The X-ray screenshot makes use of the new fix for the x-ray rendering hue shift.

--- resources/articles/meshfix/meshfix_extensive_stitching.md
+++ resources/articles/meshfix/meshfix_extensive_stitching.md
@@ -4,7 +4,30 @@ Cura needs to know where the inside of the model is in order to fill it with mat
 
 A common case of the model not being manifold, is when an extra piece of the model is attached to an otherwise closed part. This is common when editing meshes with CAD software that was not designed for manufacturing (but only for digital rendering) such as Blender or SketchUp. You can see an example of this in the images below.
 
-![The mesh looks normal on the outside](../images/meshfix_extensive_stitching_shell.png)
+<!--screenshot {
+"image_path": "meshfix_extensive_stitching_xray.png",
+"models": [{"script": "external_attachment.py"}],
+"camera_position": [62, 87, 68],
+"layer": -1
+}-->
+<!--screenshot {
+"image_path": "meshfix_extensive_stitching_disabled.png",
+"models": [{"script": "external_attachment.py"}],
+"camera_position": [62, 87, 68],
+"settings": {
+    "meshfix_extensive_stitching": false
+},
+"colours": 32
+}-->
+<!--screenshot {
+"image_path": "meshfix_extensive_stitching_enabled.png",
+"models": [{"script": "external_attachment.py"}],
+"camera_position": [62, 87, 68],
+"settings": {
+    "meshfix_extensive_stitching": true
+},
+"colours": 32
+}-->
 ![X-ray view reveals an extra surface on the inside](../images/meshfix_extensive_stitching_xray.png)
 ![With this setting disabled, only the properly closed volume gets printed](../images/meshfix_extensive_stitching_disabled.png)
 ![With this setting enabled, the extra piece gets attached properly](../images/meshfix_extensive_stitching_enabled.png)
---
+++ resources/models/external_attachment.py
@@ -0,0 +1,73 @@
+# Copyright (C) 2021 Ghostkeeper
+# This plug-in is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+# This plug-in is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for details.
+# You should have received a copy of the GNU Affero General Public License along with this plug-in. If not, see <https://gnu.org/licenses/>.
+
+import math
+
+def write_face(stream, *vertices):
+	stream.write("facet\n")
+	stream.write("outer loop\n")
+	for vertex in vertices:
+		stream.write("vertex {0} {1} {2}\n".format(*vertex))
+	stream.write("endloop\n")
+	stream.write("endfacet\n")
+
+def generate(filename):
+	"""
+	Generates an STL file representing the "External Attachment" model.
+
+	This model consists of a big and a small cube. The small cube is attached to the big cube without properly being
+	merged into one shape. The small cube has only 5 sides, leaving the face open on the side where it is attached to
+	the big cube.
+	:param filename: The file to write the model to.
+	"""
+	# Sizes of the two cubes.
+	big = 20
+	small = 10
+
+	# Pre-calculate all of the vertices.
+	small_miny = big / 2 - small / 2  # The small cube is centred along Y on the +X side of the big cube.
+	small_maxy = big / 2 + small / 2
+	small_maxx = big + small
+	big_verts = [
+		[0, 0, 0],
+		[big, 0, 0],
+		[big, big, 0],
+		[0, big, 0],
+		[0, 0, big],
+		[big, 0, big],
+		[big, big, big],
+		[0, big, big]
+	]
+	small_verts = [
+		[big, small_miny, 0],
+		[small_maxx, small_miny, 0],
+		[small_maxx, small_maxy, 0],
+		[big, small_maxy, 0],
+		[big, small_miny, small],
+		[small_maxx, small_miny, small],
+		[small_maxx, small_maxy, small],
+		[big, small_maxy, small],
+	]
+
+	# Then write all surfaces to the file.
+	with open(filename, "w") as f:
+		f.write("solid extra_surface\n")
+
+		for verts in (big_verts, small_verts):
+			write_face(f, verts[0], verts[2], verts[1])  # -Z
+			write_face(f, verts[2], verts[0], verts[3])
+			write_face(f, verts[4], verts[5], verts[6])  # +Z
+			write_face(f, verts[6], verts[7], verts[4])
+			write_face(f, verts[0], verts[1], verts[5])  # -Y
+			write_face(f, verts[5], verts[4], verts[0])
+			write_face(f, verts[3], verts[6], verts[2])  # +Y
+			write_face(f, verts[6], verts[3], verts[7])
+			if verts != small_verts:  # Leave -X face open for the small cube.
+				write_face(f, verts[0], verts[7], verts[3])  # -X
+				write_face(f, verts[0], verts[4], verts[7])
+			write_face(f, verts[1], verts[6], verts[5])  # +X
+			write_face(f, verts[1], verts[2], verts[6])
+
+		f.write("endsolid extra_surface")
